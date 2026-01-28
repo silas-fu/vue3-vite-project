03795228df719a771504ffba0834b162aa3e81bb
silas fu <rexfu0126@gmail.com>
2026-01-28 09:17:03 +0800

    modify

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/auto-imports.js",
+    }),
+    Components({
+      // 從 `./src/components/` 路徑查找
+      extensions: ["vue"],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: "src/auto-components.js",
+    }),
+
     vueDevTools(),
   ],
   resolve: {
